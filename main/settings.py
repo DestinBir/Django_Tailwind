d91df2c9b90b5de88425575b356064e20d6b1a6c
Biringanine Destin <biringaninedestin@gmail.com>
2024-12-31 16:08:00 +0200

    last commit 2024

--- main/settings.py
+++ main/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vnpjp-*=v3)!#!uf#+7^l9u^ac$#bo0kmst&ybnt9ax4^fel#6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 0
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
